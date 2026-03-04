98c9f3ee0b79a70b6b860009299011da42562b9e
derengulamahesh7@gmail.com <derengulamahesh7@gmail.com>
2026-03-04 15:51:40 +0530

    ys

---
+++ claas -9/five.js
@@ -0,0 +1,5 @@
+const arr = new Uint8Array([91,116,156,134,138,37,179,93,183]);
+let text = arr.toHex();
+
+document.getElementById("demo").innerHTML = text;
+console.log( new Uint16Array)
---
+++ claas -9/six.js
@@ -0,0 +1,4 @@
+let text = '5b74c868a25b35db7';
+const arr = Uint8Array.fromhex(text);
+document.getElementById("demo").innerHTML=arr;
+console.log(text)
